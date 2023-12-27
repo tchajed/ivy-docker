33c79be28ac9f85efc6971ae1de10f5e3868bfa0
Tej Chajed <tchajed@gmail.com>
2023-12-27 08:05:46 -0600

    Improve the uninstall process

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-apt-get remove -y g++ git
+set -eu
+
+# we need gcc for some C++ libraries that Z3 depends on;
+# this marks it manually installed
+apt-get install -y gcc
+apt-get remove -y g++ git cmake
 apt-get autoremove -y
 apt-get clean
+rm -rf /var/lib/apt/lists/*
+# make sure z3 still works
+python3 -c 'import ivy.z3'
 rm -rf ivy
+rm set-locale.sh install.sh uninstall.sh
