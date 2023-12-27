520c046e968109054d54f566bb6650a0ef329fb9
Tej Chajed <tchajed@gmail.com>
2023-12-27 08:35:08 -0600

    Combine install/uninstall to reduce image size

--- Dockerfile
+++ Dockerfile
@@ -9,9 +9,10 @@ ENV LANG=en_US.UTF-8 \
          LC_ALL=en_US.UTF-8
 
 COPY install.sh .
-RUN bash install.sh
-
 COPY uninstall.sh .
-RUN bash uninstall.sh
+RUN ./install.sh
+
+#COPY uninstall.sh .
+#RUN bash uninstall.sh
 
 ENTRYPOINT ["/usr/local/bin/ivy_check"]
--- install.sh
+++ install.sh
@@ -7,10 +7,13 @@ apt-get install -y python python3 python3-pip g++ cmake \
   git pkg-config libssl-dev libreadline-dev
 pip3 install pyparsing pexpect
 
-git clone --recurse-submodules https://github.com/kenmcmil/ivy.git
+git clone --depth=1 --recurse-submodules https://github.com/kenmcmil/ivy.git
 cd ivy
-sed -i "s/make -j 4/make -j8/" build_submodules.py
-sed -i "s/'make'/'make -j8'/" build_submodules.py
-sed -i "s/&& make/\\0 -j8/" build_submodules.py
+sed -i "s/make -j 4/make -j$(nproc)/" build_submodules.py
+sed -i "s/'make'/'make -j$(nproc)'/" build_submodules.py
+sed -i "s/&& make/\\0 -j$(nproc)/" build_submodules.py
 python3 build_submodules.py
 python3 setup.py install
+
+cd ..
+./uninstall.sh
